3a2fa38cddc4099e49de2a6e248ec472843075c5
Jangam Naveen <jangamnaveen291@gmail.com>
2025-05-29 17:04:51 +0530

    handling the problem of server crashing when it got random input for lets say a client or a tester

--- major project/app.js
+++ major project/app.js
@@ -7,6 +7,8 @@ const path = require("path");
 const listing = require("./models/listing.js");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js")
 
 
 main().then(() => {
@@ -27,9 +29,6 @@ app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 
-app.listen(8080,() => {
-    console.log("server is listeningh to port 8080");
-});
 app.get("/",(req, res)=>{
     res.send("Hi,Iam root");
 });
@@ -43,32 +42,49 @@ app.get("/",(req, res)=>{
         res.render("listing/new");
     });
     //show route
-    app.get("/listings/:id",async (req, res) => {
+    app.get("/listings/:id",wrapAsync( async (req, res) => {
         let {id} = req.params;
         const listing = await Listing.findById(id);
         res.render("listing/show",{ listing });
-    });
+    }));
     //create route 
-    app.post("/listings",async (req, res)=>{
-        const newListing = new Listing(req.body.listing);
-        await newListing.save();
-        res.redirect("/listings");
-    });
+    app.post("/listings",wrapAsync( async (req, res)=>{
+        if(!req.body.listings){
+            throw new ExpressError(400,"Send valid data for listing");
+        }
+            const newListing = new Listing(req.body.listing);
+            await newListing.save();
+            res.redirect("/listings");
+    }));
     //Edit route
-    app.get("/listings/:id/edit", async (req, res)=> {
+    app.get("/listings/:id/edit", wrapAsync( async (req, res)=> {
         let {id} = req.params;
         const listing = await Listing.findById(id);
         res.render("listing/edit.ejs",{listing});
-    });
+    }));
     //update route 
-    app.put("/Listings/:id",async (req, res) => {
+    app.put("/Listings/:id",wrapAsync( async (req, res) => {
         let {id} = req.params;
         await Listing.findByIdAndUpdate(id,{ ...req.body.listing });
         res.redirect("/listings")      
-    });
-    app.delete("/listings/:id",async (req, res) => {
+    }));
+    app.delete("/listings/:id",wrapAsync( async (req, res) => {
         let {id} = req.params;
         let deletedListing = await Listing.findByIdAndDelete(id);
         console.log(deletedListing);
         res.redirect("/listings");        
+    }));
+
+    app.all("*",(req,res,next)=>{
+        next(new ExpressError(404,"Page Not Found!!!"));
+    });
+
+    app.use((err,req,res,next) =>{
+        let {statusCode=500,message="There was an error on our side"}= err;
+        res.status(statusCode).send(message);
+    });
+
+
+    app.listen(8080,() => {
+        console.log("server is listeningh to port 8080");
     });
